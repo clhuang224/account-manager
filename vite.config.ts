ac83906a8163fd56ce1b51af17ba213a0ded24e9
Lynn <clhuang224@gmail.com>
2026-06-19 13:25:01 +0800

    chore: add quasar

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { join } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: join(import.meta.dirname, 'src/quasar-variables.sass'),
+    }),
     vueDevTools(),
   ],
   resolve: {
